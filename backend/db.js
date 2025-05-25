25ba5fc030a07697e204aa02f05719747c4accf3
Aditya <adityajhunjhunwala13@gmail.com>
2025-05-25 19:34:10 +0530

    hosted db server on atlas

--- backend/db.js
+++ backend/db.js
@@ -1,8 +1,9 @@
 const mongoose = require("mongoose");
+require("dotenv").config();
 
-const mongoURI =
-  "mongodb://localhost:27017/inotebook?readPreference=primary&appname=MongoDB%20Compass&ssl=false";
-
+const mongoURI = process.env.MONGODB_URL; //For Remote:ON MongoDB Atlas
+// const mongoURI =
+//   "mongodb://localhost:27017/inotebook?readPreference=primary&appname=MongoDB%20Compass&ssl=false";
 const connectToMongo = () => {
   mongoose.connect(mongoURI, () => {
     console.log("Connected to Mongo Successfully");
